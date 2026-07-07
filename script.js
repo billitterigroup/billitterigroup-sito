d2cccf480312140799455f434d4c8c923f47415d
billitterigroup <billitterigroup@gmail.com>
2026-07-07 13:03:43 +0200

    Add files via upload
    HOME PAGE DEFINITIVA

---
+++ script.js
@@ -0,0 +1,5 @@
+// Billitteri Group - Homepage definitiva
+// Le sezioni successive saranno ampliate con Galleria, Prima/Dopo, Servizi e SEO.
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('loaded');
+});
