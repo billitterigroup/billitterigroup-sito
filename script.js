5817fed01a018fa399e0dc7ed2de93cb1343306f
billitterigroup <billitterigroup@gmail.com>
2026-07-07 19:57:53 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Billitteri Group - Homepage definitiva
-// Le sezioni successive saranno ampliate con Galleria, Prima/Dopo, Servizi e SEO.
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.classList.add('loaded');
-});
